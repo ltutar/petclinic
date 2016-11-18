ad7e1cee58005083f499d80165945e14f0c0f72e
Gerard Borst <gerard.borst@rabobank.nl>
2016-11-18 19:09:05 +0100

    hudson setting werkt niet

--- jenkins/init_jenkins.groovy
+++ jenkins/init_jenkins.groovy
@@ -7,16 +7,9 @@ import org.jenkinsci.plugins.workflow.multibranch.*
 
 def instance = Jenkins.getInstance()
 
-println "--> set Maven installation"
-def extList = instance.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0];
-def tmpList = (extList.installations as List);
-tmpList.add(new hudson.tasks.Maven.MavenInstallation("Maven 3", "/opt/mvn", []));
-extList.installations = tmpList
-extList.save()
-
 println "--> set Multi Branch Project"
 
 WorkflowMultiBranchProject mp = instance.createProject(WorkflowMultiBranchProject.class, "petclinic");
 mp.getSourcesList().add(new BranchSource(
-	new GitSCMSource(null, "https://github.com/ltutar/petclinic", "", "*", "", false), new DefaultBranchPropertyStrategy(new BranchProperty[0])));
+	new GitSCMSource(null, "https://github.com/gerardborst/petclinic", "", "*", "", false), new DefaultBranchPropertyStrategy(new BranchProperty[0])));
 mp.scheduleBuild2(0).getFuture().get()
